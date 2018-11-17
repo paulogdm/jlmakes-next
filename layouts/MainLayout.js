2dce2c0fa442cef1810b2357d0861dc85bc57135
Julian Lloyd <julianlloyd@gmail.com>
2018-11-16 16:14:10 -0800

    Implement sticky footer

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,10 +1,64 @@
 import Header from '../components/Header';
 
-const MainLayout = ({children}) => (
-  <>
-    <Header />
-    {children}
-  </>
-);
+function MainLayout({children}) {
+  return (
+    <>
+      <Wrapper>
+        <Header />
+        <Main>{children}</Main>
+        <Footer>This is the footer.</Footer>
+      </Wrapper>
+    </>
+  );
+}
+
+function Wrapper({children}) {
+  return (
+    <>
+      <div>{children}</div>
+      <style jsx>
+        {`
+          div {
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+function Main({children}) {
+  return (
+    <>
+      <main>{children}</main>
+      <style jsx>
+        {`
+          main {
+            flex: 1 0 auto;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+function Footer({children}) {
+  return (
+    <>
+      <footer>{children}</footer>
+      <style jsx>
+        {`
+          footer {
+            flex-shrink: 0;
+            height: 240px;
+            background: #222;
+          }
+        `}
+      </style>
+    </>
+  );
+}
 
 export default MainLayout;
