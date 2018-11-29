5dde14da35a8217ebd6f495795bd582f19c4871d
Julian Lloyd <julianlloyd@gmail.com>
2018-11-28 16:18:46 -0800

    Refactor Canvas with PureComponent

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 
-class Canvas extends Component {
+class Canvas extends PureComponent {
   ref = React.createRef();
   state = {
     width: 0,
