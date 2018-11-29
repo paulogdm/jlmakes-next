7e804d5c1d05b29b993d718fe3cc23f6411a0c7f
Julian Lloyd <julianlloyd@gmail.com>
2018-11-29 00:45:06 -0800

    Add Vignette to Index

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import Canvas from '../components/Canvas';
 import Particles from '../components/Particles';
+import Vignette from '../components/Vignette';
 
 function Index() {
   return (
     <MainLayout>
+      <Vignette />
       <Canvas>
         {(ctx, dimensions) => (
           <Particles count={600} ctx={ctx} dimensions={dimensions} />
