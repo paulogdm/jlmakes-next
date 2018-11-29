c144e6d0fdb1a7972a4661093b1c887f45a29866
Julian Lloyd <julianlloyd@gmail.com>
2018-11-29 00:44:02 -0800

    Create Vignette

---
+++ components/Vignette.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Vignette = () => (
+  <>
+    <div className="vignette" />
+    <style jsx>
+      {`
+        .vignette {
+          position: fixed;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          z-index: 2;
+          background: transparent;
+          background: radial-gradient(transparent, 60%, #180818);
+        }
+      `}
+    </style>
+  </>
+);
+
+export default Vignette;
