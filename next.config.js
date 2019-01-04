5aec2f31c68f371ed8ac554eec89123ecafb0893
paulogdm <paulogdemitri@gmail.com>
2019-01-04 16:49:06 -0200

    v2 draft
    We may need to change how next.config.js looks!

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+    ? require('next/constants')
+    : require('next-server/constants');
 
-const analyzer = new BundleAnalyzerPlugin({
-  analyzerMode: 'server',
-  analyzerPort: 8888,
-  openAnalyzer: true,
-});
+module.exports = phase => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {'target': 'serverless'};
+  }
 
-module.exports = {
-  webpack(config) {
-    if (process.env.analyze) {
-      config.plugins.push(analyzer);
+  const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+  const analyzer = {
+    analyzerMode: 'server',
+    analyzerPort: 8888,
+    openAnalyzer: true,
+  };
+
+  return {
+    webpack: config => {
+      if (process.env.analyze) {
+        config.plugins.push(new BundleAnalyzerPlugin(analyzer));
+      }
+      return config;
     }
-
-    return config;
-  },
+  };
 };
