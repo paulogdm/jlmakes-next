5c1f6ee8bcf89b989322d2090c9c4fd69899cdb1
paulogdm <paulogdemitri@gmail.com>
2019-01-04 18:44:33 -0200

    No need for PHASE_PRODUCTION_SERVER

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW_REGION
-    ? require('next/constants')
-    : require('next-server/constants');
-
 module.exports = phase => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
+  if (process.env.NOW_REGION) {
     return {'target': 'serverless'};
   }
 
